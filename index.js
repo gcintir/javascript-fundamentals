057a14e14e04aef6618175e01aec6b3d69b8eac4
DIR\guray.cintir <guray.cintir@accenture.com>
2022-05-27 09:28:28 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let price = .99;
+console.log(price); // prints 0.99
+
+let amount = 0.2 + 0.1
+console.log(amount); // prints 0.30000000000000004
+console.log(amount.toFixed(2)); // prints 0.30
+console.log(amount.toString()); // converts to string
+
+let pageSize = 9007199254740991n;
+console.log(pageSize);
+
+let person = {
+    name : "John",
+    surname: "Doe"
+};
+console.log(person.name, person['surname']);
+person.age = 22;
+console.log(person);
+delete person.age;
+console.log('age' in person);
+
+let arr = Array(1,2,3);
+console.log(arr);
+console.log(arr.length);
+let emptyArr = [];
+console.log(Array.isArray(emptyArr));
+arr.push(5);
+arr.pop();
+arr.unshift(0);
+arr.shift();
+console.log(arr);
+
+const profile = null;
+const userMode = profile ?? 'admin'; // ?? nullish coalescing operator
+console.log(userMode);
