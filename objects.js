6b7343ef0528e05a2f49afdd65b22f12a9643688
DIR\guray.cintir <guray.cintir@accenture.com>
2022-05-30 17:22:14 +0300

    object samples

---
+++ objects.js
@@ -0,0 +1,30 @@
+let person = {
+    name : "John",
+    surname : "Taylor",
+    greeting : function () {
+        console.log(this.name, this.surname);
+    }
+};
+person.greeting();
+
+function Person (firstName, lastName) {
+
+    // this = {}
+
+    if (!new.target) {
+        throw Error("Cannot be called without the new keyword");
+    }
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getFullName = function() {
+        return firstName + " " + lastName;
+    }
+
+    // return this object
+}
+let p1 = new Person("a", "b");
+let p2 = new Person("x", "y");
+console.log(p1.getFullName());
+console.log(p2.getFullName());
+
+
