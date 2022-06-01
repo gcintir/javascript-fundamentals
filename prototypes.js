caa7af38a1f8bc4a6f8851d18f8233cbe5714519
DIR\guray.cintir <guray.cintir@accenture.com>
2022-06-01 15:58:44 +0300

    class keyword example

--- prototypes.js
+++ prototypes.js
@@ -23,3 +23,18 @@ p1.greet = function() {
     return "Hello";
 };
 console.log(p1.greet());
+
+class Computer {
+    constructor(cpu, ram) {
+        this.cpu = cpu;
+        this.ram = ram;
+    }
+
+    getInfo () {
+        return "cpu: " + this.cpu + " ram: "  + this.ram;
+    }
+};
+let desktop = new Computer("4", "4");
+let laptop = new Computer("8", "8");
+console.log(desktop.getInfo());
+console.log(laptop.getInfo());
